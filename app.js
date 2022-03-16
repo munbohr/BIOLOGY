eae5f806c1093eda0da33ba77e2958da33480e03
munbohr <85949748+munbohr@users.noreply.github.com>
2022-03-16 20:11:16 +0900

    add lee img & change design & add or remove Text

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const title = document.querySelector(".center-contents h1")
+const img = document.querySelector("btn");
+const imgForm = document.querySelector("#form button");
+const imgFormImg = document.querySelector("#form img");
+const Form = document.querySelector("#form");
+imgFormImg.classList.add("hidden");
+
 
 const handleClick = {
     hover: function() {
@@ -7,10 +13,16 @@ const handleClick = {
         title.classList.toggle = "응애";
         console.log(title);
         title.style.color = "skyblue";
-
-        
     }
 
 }
 
+function handleClickImg(event) {
+    event.preventDefault();
+    imgForm.classList.add("hidden");
+    if(imgFormImg === "hidden") {}
+    imgFormImg.classList.toggle("appear")
+}
+
 title.addEventListener("click", handleClick.hover);
+Form.addEventListener("submit", handleClickImg);
